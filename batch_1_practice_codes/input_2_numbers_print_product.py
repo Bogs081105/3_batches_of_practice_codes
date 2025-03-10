3af9bb60e022d3d882b2777fd10a5a62ef26a931
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 21:41:37 +0800

    added input 2 numbers function

--- batch_1_practice_codes/input_2_numbers_print_product.py
+++ batch_1_practice_codes/input_2_numbers_print_product.py
@@ -1,3 +1,6 @@
 #input 2 numbers
+num1 = float(input("Enter the first : "))
+num2 = float(input("Enter the second number: "))
+
 #multiply the 2 numbers
-#print the product of the 2 numbers 
+#print the product of the 2 numbers
