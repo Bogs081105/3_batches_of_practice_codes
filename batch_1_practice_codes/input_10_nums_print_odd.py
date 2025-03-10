9001a8ae937e7d3e59a8e7743f110d2406da98b9
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 22:44:33 +0800

    created pseudo code

---
+++ batch_1_practice_codes/input_10_nums_print_odd.py
@@ -0,0 +1,4 @@
+#ask the user to input 10 numbers, used for loop for convenience
+#set the total to 0, then for every input, add 1 input until it reaches 10 inputs
+#define the odd numbers
+#print the number of odd numbers
