8269b76f9c9b1f3638a34ab5b34199d5f4b3f5c9
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 22:25:43 +0800

    added addition of all numbers function

--- batch_1_practice_codes/input_10_numbers_print_sum.py
+++ batch_1_practice_codes/input_10_numbers_print_sum.py
@@ -2,7 +2,9 @@
 #set the total to 0, then for every input, add 1 input until it reaches 10 inputs
 total = 0
 for i in range (10):
-    num = float(input(f"Enter number{i+1}:"))
+    num = float(input(f"Enter number{i+1}: "))
 
 #add all the numbers
+    total += num
+
 #print the sum of the 10 numbers
