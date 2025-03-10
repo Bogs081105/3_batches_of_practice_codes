6ce338c36dfed69ffedc60ded5fda8b09de999f5
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 22:10:20 +0800

    created pseudo code

---
+++ batch_1_practice_codes/input_10_numbers_print_sum.py
@@ -0,0 +1,4 @@
+#ask the user to input 10 numbers, used for loop for convenience
+#set the total to 0, then for every input, add 1 input until it reaches 10 inputs
+#add all the numbers
+#print the sum of the 10 numbers
