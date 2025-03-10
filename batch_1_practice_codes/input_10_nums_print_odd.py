da4ec8b7ea10b3d0294121773d0002c92d8d9d4b
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 22:46:34 +0800

    added insert 10 numbers function using for loop

--- batch_1_practice_codes/input_10_nums_print_odd.py
+++ batch_1_practice_codes/input_10_nums_print_odd.py
@@ -1,4 +1,8 @@
 #ask the user to input 10 numbers, used for loop for convenience
 #set the total to 0, then for every input, add 1 input until it reaches 10 inputs
+total = 0
+for i in range (10):
+    num = float(input(f"Enter number{i+1}: "))
+
 #define the odd numbers
 #print the number of odd numbers
