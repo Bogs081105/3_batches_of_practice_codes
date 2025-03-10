8e33ed361570bc9c6a95494dd4a3581fa48d5746
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 22:51:02 +0800

    added a function to count and define odd numbers

--- batch_1_practice_codes/input_10_nums_print_odd.py
+++ batch_1_practice_codes/input_10_nums_print_odd.py
@@ -4,5 +4,7 @@ total = 0
 for i in range (10):
     num = float(input(f"Enter number{i+1}: "))
 
-#define the odd numbers
+#define the odd numbers and add the count for odd numbers
+if num % 2 != 0:
+    total += 1
 #print the number of odd numbers
