60ee03be9287202c765e50c3617f349bf66100f8
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-10 21:15:39 +0800

    added insert 2 numbers option

--- batch_1_practice_codes/input_2_numbers_print_bigger_number.py
+++ batch_1_practice_codes/input_2_numbers_print_bigger_number.py
@@ -1,3 +1,6 @@
 #ask the user to input 2 numbers
+num1 = float(input("Input the first number: "))
+num2 = float(input("Input the second number: "))
+
 #identify the bigger number
 #print the bigger number
