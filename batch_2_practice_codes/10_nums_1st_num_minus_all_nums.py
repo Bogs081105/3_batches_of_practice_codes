d011326fd1a1ce55bd9fa94952cceab847c3fd4e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 02:57:15 +0800

    added a slice function in the index to get the 2nd number only up until the last number, then added a subtraction function

--- batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
+++ batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
@@ -5,7 +5,11 @@ for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
 #added append function to add the numbers into the empty set
     nums.append(num)
-#program it to subtract all of the remaining numbers to the first number
 #added a first num function which only gets the first number inside the index
 first_num = nums[0]
+#program it to subtract all of the remaining numbers to the first number
+#added a slice function to only get the 2nd number up until the last number
+for num in nums[1:]:
+    first_num -= num
+
 #print the result
