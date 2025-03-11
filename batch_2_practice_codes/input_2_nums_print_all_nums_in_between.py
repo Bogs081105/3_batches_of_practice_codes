d12720640c09d846d00df818376b956f0b6fdc33
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 03:53:18 +0800

    fixed error wherein float couldnt convert into int. Changed it into int(input). Added print function

--- batch_2_practice_codes/input_2_nums_print_all_nums_in_between.py
+++ batch_2_practice_codes/input_2_nums_print_all_nums_in_between.py
@@ -1,7 +1,7 @@
 #insert 2 numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 #using for loop, insert the first number and the 2nd number as the start and stop
-for i in range(num1, num2):
-    print(i)
+for i in range(num1+1, num2):
 #print the numbers
+    print(i)
