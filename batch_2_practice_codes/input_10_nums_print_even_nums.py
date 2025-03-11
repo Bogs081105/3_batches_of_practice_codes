0eef8e143b657d840e08e288b3813c63ce5f3680
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 03:17:50 +0800

    added print function + fixed an indentation error which doesnt count the even numbers

--- batch_2_practice_codes/input_10_nums_print_even_nums.py
+++ batch_2_practice_codes/input_10_nums_print_even_nums.py
@@ -5,6 +5,7 @@ for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
 #function to count even numbers
 #for every number that is divisible by 2(even numbers are also divisible by 2) add into total
-if num % 2 == 0:
-    total += 1
+    if num % 2 == 0:
+        total += 1
 #print the amount of even numbers
+print(f"The number of even numbers is/are: {total}")
