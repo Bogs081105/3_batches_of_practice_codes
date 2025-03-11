9af6a54b9f553d00420befff0677b381b34a4b1f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-11 12:01:09 +0800

    added input 2 numbers function

--- batch_2_practice_codes/2_nums_get_quotient_no_decimal.py
+++ batch_2_practice_codes/2_nums_get_quotient_no_decimal.py
@@ -1,3 +1,6 @@
 #input 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 #get the quotient of the 2 numbers without the decimal
 #print the result
