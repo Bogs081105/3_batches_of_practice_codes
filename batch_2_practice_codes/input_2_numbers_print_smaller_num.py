5d20fe2e7cb0c219569e04d333c7a06b1af4098d
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-11 11:36:45 +0800

    created insert 2 numbers function

--- batch_2_practice_codes/input_2_numbers_print_smaller_num.py
+++ batch_2_practice_codes/input_2_numbers_print_smaller_num.py
@@ -1,3 +1,6 @@
 #ask the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 #see which number is the smaller number
 #print the smaller number
