c36ad45122d4f525a418e864d381f0698742a8d0
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 05:33:56 +0800

    added an empty list to store numbers

---
+++ batch_3_practice_codes/10_nums_displaying_no_duplicates.py
@@ -0,0 +1,6 @@
+#ask the user to input 10 nums using for loop
+#added an empty set
+nums = []
+for i in range (10):
+    num = int(input(f"Enter number{i+1}: "))
+#check if the numbers have duplicates
