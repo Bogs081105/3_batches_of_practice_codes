8ee33f7c81feb00086ff8b3ed9acae2d236e2c76
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 03:06:39 +0800

    added input 10 numbers function using for loop

--- batch_2_practice_codes/input_10_nums_print_even_nums.py
+++ batch_2_practice_codes/input_10_nums_print_even_nums.py
@@ -1,5 +1,7 @@
 #set total to 0 to represent the start of the count for even numbers
 total = 0
 #input 10 numbers using for loop
+for i in range(10):
+    num = float(input(f"Enter number {i + 1}: "))
 #function to count even numbers
 #print the amount of even numbers
