105a56b82a6cad7f7fc12e76c12d889e182b67fc
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-11 12:07:37 +0800

    changed the / to // to show no decimal, added print function

--- batch_2_practice_codes/2_nums_get_quotient_no_decimal.py
+++ batch_2_practice_codes/2_nums_get_quotient_no_decimal.py
@@ -1,6 +1,9 @@
 #input 2 numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 #get the quotient of the 2 numbers without the decimal
+quotient = (num1 // num2)
+
 #print the result
+print(f"{num1} / {num2} = {quotient}")
