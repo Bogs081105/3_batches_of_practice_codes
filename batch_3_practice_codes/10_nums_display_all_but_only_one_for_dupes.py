b42343aa28e5fa748bfb708148a40e8598e5e291
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:04:47 +0800

    created an empty list

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -1,4 +1,5 @@
 #create an empty list to append the numbers
+nums = 0
 #ask the user to input 10 numbers
 #create a function that displays all nums but only 1 copy of nums that has dupes
 #print all the numbers
