55ffb1044f832b703684c44336d0914029f3ff7e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:35:22 +0800

    fixed an error that shows multiple copies of duplicates by moving append to the if not function

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -5,9 +5,9 @@ dupes = set()
 #ask the user to input 10 numbers
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
-    nums.append(num)
 #create a function that displays all nums but only 1 copy of nums that has dupes
 if num not in dupes:
+    nums.append(num)
     dupes.add(num)
     
 #display the numbers
