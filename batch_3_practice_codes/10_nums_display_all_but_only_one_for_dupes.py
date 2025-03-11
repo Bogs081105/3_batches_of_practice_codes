de5326524e34885ccb1dba88ca55b1aef38f870f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:29:11 +0800

    removed the past codes, added a new set to input the duplicate numbers

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -1,11 +1,9 @@
 #create an empty list to append the numbers
 nums = [0]
+#created an empty set to store duplicates.
+dupes = set()
 #ask the user to input 10 numbers
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     nums.append(num)
 #create a function that displays all nums but only 1 copy of nums that has dupes
-repeating = [num for num in nums if nums.count(num) != 1]
-nums.append(repeating)
-
-print(nums)
