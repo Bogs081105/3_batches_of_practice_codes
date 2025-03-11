4b9d9dda5bce55cc642a6833addc9d61059b702d
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 03:51:25 +0800

    added the for loop to count 1st num to 2nd num

--- batch_2_practice_codes/input_2_nums_print_all_nums_in_between.py
+++ batch_2_practice_codes/input_2_nums_print_all_nums_in_between.py
@@ -1,6 +1,7 @@
 #insert 2 numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-
 #using for loop, insert the first number and the 2nd number as the start and stop
+for i in range(num1, num2):
+    print(i)
 #print the numbers
