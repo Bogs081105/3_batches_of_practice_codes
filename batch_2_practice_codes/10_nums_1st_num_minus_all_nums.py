d99de34741a21561b3f0aa02b875a0df3a556f91
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 02:50:42 +0800

    added a first number function which gets the first number of the index

--- batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
+++ batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
@@ -6,10 +6,6 @@ for i in range(10):
 #added append function to add the numbers into the empty set
     nums.append(num)
 #program it to subtract all of the remaining numbers to the first number
-#added first num function for the first number
+#added a first num function which only gets the first number inside the index
 first_num = nums[0]
-#used slice 1 up until the last number to subtract the first num and the numbers
-for num in nums[1:]:
-    first_num -= num
-
 #print the result
