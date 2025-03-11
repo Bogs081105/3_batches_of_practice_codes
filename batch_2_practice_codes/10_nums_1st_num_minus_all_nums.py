bfae9454c846144a3757e4e8d41c0d38f0b7c91a
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 02:49:14 +0800

    added an append function to add the numbers into the empty set

--- batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
+++ batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
@@ -1,5 +1,15 @@
 #using for loop, input 10 numbers
+#added an empty set to store the numbers
+nums = []
 for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
+#added append function to add the numbers into the empty set
+    nums.append(num)
 #program it to subtract all of the remaining numbers to the first number
-#print the result 
+#added first num function for the first number
+first_num = nums[0]
+#used slice 1 up until the last number to subtract the first num and the numbers
+for num in nums[1:]:
+    first_num -= num
+
+#print the result
