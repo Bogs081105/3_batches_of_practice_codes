f6b86adf4640b84bdb65542e32c8407950c4f957
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 05:57:07 +0800

    fixed the function to iterate the integers properly

--- batch_3_practice_codes/10_nums_displaying_no_duplicates.py
+++ batch_3_practice_codes/10_nums_displaying_no_duplicates.py
@@ -3,6 +3,9 @@
 nums = []
 for i in range (10):
     num = int(input(f"Enter number{i+1}: "))
-#check if the numbers have duplicates
 #append the numbers first in the list
 nums.append(num)
+#check if the numbers have duplicates
+#set a new list named no duplicates to insert the numbers with no duplicates
+#this checks each numbers if it is the only number inside the list.
+no_duplicates = [num for num in nums if nums.count(num) == 1]
