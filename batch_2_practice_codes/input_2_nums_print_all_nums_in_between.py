28f537be7fd28ca0739e487ff167c760d0b0455c
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 03:49:40 +0800

    added insert 2 nums function

--- batch_2_practice_codes/input_2_nums_print_all_nums_in_between.py
+++ batch_2_practice_codes/input_2_nums_print_all_nums_in_between.py
@@ -1,3 +1,6 @@
 #insert 2 numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 #using for loop, insert the first number and the 2nd number as the start and stop
 #print the numbers
