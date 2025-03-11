fbd65e5735ff0ef92a3ca667ac10e805f78a7f1b
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:07:49 +0800

    added append function

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -3,6 +3,6 @@ nums = [0]
 #ask the user to input 10 numbers
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
-    
+    nums.append(num)
 #create a function that displays all nums but only 1 copy of nums that has dupes
 #print all the numbers
