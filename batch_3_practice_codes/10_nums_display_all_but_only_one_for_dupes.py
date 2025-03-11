cc4def42f7d860379654bbe4a1b0824167c14c7a
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:06:20 +0800

    added insert 10 nums function

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -1,5 +1,8 @@
 #create an empty list to append the numbers
 nums = 0
 #ask the user to input 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    
 #create a function that displays all nums but only 1 copy of nums that has dupes
 #print all the numbers
