f99b03d76ec6dd0cf9f99ead72aac1cc1838c5f9
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:04:24 +0800

    created a pseudo code

---
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -0,0 +1,4 @@
+#create an empty list to append the numbers
+#ask the user to input 10 numbers
+#create a function that displays all nums but only 1 copy of nums that has dupes
+#print all the numbers
