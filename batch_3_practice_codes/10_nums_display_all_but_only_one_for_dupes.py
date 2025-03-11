5ae256c6116768a6a0d610a38060b74c5856ca2f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:22:01 +0800

    added a repeating numbers function

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -5,4 +5,6 @@ for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     nums.append(num)
 #create a function that displays all nums but only 1 copy of nums that has dupes
+repeating = [num for num in nums if nums.count(num) != 1]
 #print all the numbers
+print(repeating)
