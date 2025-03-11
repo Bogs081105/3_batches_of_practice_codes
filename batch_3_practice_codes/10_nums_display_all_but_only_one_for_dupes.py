c382960e5a3f0b6e754a96e11bf7256502ee98a7
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 06:43:04 +0800

    fixed an indentation error which only shows the last number

--- batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
+++ batch_3_practice_codes/10_nums_display_all_but_only_one_for_dupes.py
@@ -6,9 +6,9 @@ dupes = set()
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
 #create a function that displays all nums but only 1 copy of nums that has dupes
-if num not in dupes:
-    nums.append(num)
-    dupes.add(num)
+    if num not in dupes:
+        nums.append(num)
+        dupes.add(num)
     
 #display the numbers
 print(nums)
--- batch_3_practice_codes/10_nums_displaying_no_duplicates.py
+++ batch_3_practice_codes/10_nums_displaying_no_duplicates.py
@@ -4,11 +4,9 @@ nums = []
 for i in range (10):
     num = int(input(f"Enter number{i+1}: "))
 #append the numbers first in the list
-    nums.append(num)
+nums.append(num)
 #check if the numbers have duplicates
 #set a new list named no duplicates to insert the numbers with no duplicates
 #this checks each numbers if it is the only number inside the list.
 no_duplicates = [num for num in nums if nums.count(num) == 1]
-
-#print the result
-print(no_duplicates)
+print(f"The numbers with no duplicates is/are: {no_duplicates}")
