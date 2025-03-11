308c12ee90bf487759d2b90bb510a2ee4b54ac04
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-11 11:57:33 +0800

    added difference function

--- batch_2_practice_codes/2_numbers_print_difference.py
+++ batch_2_practice_codes/2_numbers_print_difference.py
@@ -3,4 +3,5 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 #get the difference of the 2 numbers
+difference = (num1 - num2)
 #print the difference
