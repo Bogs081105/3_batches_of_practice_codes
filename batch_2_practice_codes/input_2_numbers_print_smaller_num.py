fe7ff54229fcc2650af37a93cfda4b13bb5558b3
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-11 11:46:03 +0800

    created identification of the smaller number, and print function

--- batch_2_practice_codes/input_2_numbers_print_smaller_num.py
+++ batch_2_practice_codes/input_2_numbers_print_smaller_num.py
@@ -4,3 +4,9 @@ num2 = float(input("Enter the second number: "))
 
 #see which number is the smaller number
 #print the smaller number
+if num1 < num2:
+    print(f"{num1} is the smaller number")
+elif num2 < num1:
+    print(f"{num2} is the smaller number")
+else:
+    print("the numbers are equal")
