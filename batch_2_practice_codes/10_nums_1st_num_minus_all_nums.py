c3e7397e31b6c0ceb52c9e15f1c8a671c60dc5c2
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 02:33:19 +0800

    added insert 10 numbers function

--- batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
+++ batch_2_practice_codes/10_nums_1st_num_minus_all_nums.py
@@ -1,3 +1,5 @@
 #using for loop, input 10 numbers
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
 #program it to subtract all of the remaining numbers to the first number
 #print the result 
