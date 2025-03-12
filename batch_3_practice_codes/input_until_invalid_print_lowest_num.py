c2cbb1e6c76d748943c22e548e0276eef59e7209
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 17:40:35 +0800

    created pseudo code

---
+++ batch_3_practice_codes/input_until_invalid_print_lowest_num.py
@@ -0,0 +1,5 @@
+#set a variable to store the lowest numberw
+#using while loop, input numbers continuously
+#create a function that identifies which is the lowest number
+#break if it is an invalid number
+#print the lowest number
