db013eebc95f8d988fbd5c94d55f7226f9556f2c
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 16:39:22 +0800

    added the function where it tries to convert the number into float, then identify if it is duplicate or unique.

--- batch_3_practice_codes/display_unique_and_duplicate.py
+++ batch_3_practice_codes/display_unique_and_duplicate.py
@@ -2,7 +2,14 @@
 seen_nums = set()
 #using while loop, continuously ask for the user until invalid input
 while True:
-    input = input("Please enter a number: ")
+    user_input = input("Please enter a number: ")
 #check if the number is a duplicate or unique
+try:
+    num = float(user_input)
+    if num in seen_nums:
+        print("Duplicate")
+    else:
+        print("Unique")
+        seen_nums.add(num)
 #check if the input is a valid number
 #print invalid if the number is invalid
