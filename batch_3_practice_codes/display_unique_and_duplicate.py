89e5c2705861cc00d8426634a220948e4dfb9552
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 17:13:04 +0800

    fixed error and added exit function

--- batch_3_practice_codes/display_unique_and_duplicate.py
+++ batch_3_practice_codes/display_unique_and_duplicate.py
@@ -2,18 +2,21 @@
 seen_nums = set()
 #using while loop, continuously ask for the user until invalid input
 while True:
-    user_input = input("Please enter a number: ")
-    if ValueError:
+    user_input = input("Please enter a number(or type 'exit' to quit): ")
+
+    if user_input.lower() == 'exit':
         break
 #check if the number is a duplicate or unique
-try:
-    num = float(user_input)
-    if num in seen_nums:
-        print("Duplicate")
-    else:
-        print("Unique")
-        seen_nums.add(num)
+    try:
+        num = float(user_input)
+        
+        if num in seen_nums:
+            print("Duplicate")
+        else:
+            print("Unique")
+            seen_nums.add(num)
 #check if the input is a valid number
 #print invalid if the number is invalid
-except ValueError:
-    print("Invalid input.")
+    except ValueError:
+        print("Invalid input.")
+        break
