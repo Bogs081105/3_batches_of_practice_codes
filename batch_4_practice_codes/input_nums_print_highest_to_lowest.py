9874bb5244b332934ff188d0d3fc70662806c976
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 22:00:52 +0800

    added a function to input numbers continuously

--- batch_4_practice_codes/input_nums_print_highest_to_lowest.py
+++ batch_4_practice_codes/input_nums_print_highest_to_lowest.py
@@ -1,6 +1,9 @@
 #create an empty list to store numbers
 num = []
 #input continuously using for loop
+while True:
+    try:
+        num = int(input("Enter a number(input an invalid item to stop): "))
 #append numbers into list
 #check if invalid input
 #sort in descending order
