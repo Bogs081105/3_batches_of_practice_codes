de192fa0abb6940949fd272ccf02a28b0f81ded8
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 16:23:35 +0800

    pseudo code created

---
+++ batch_3_practice_codes/display_unique_and_duplicate.py
@@ -0,0 +1,5 @@
+#use set function to store numbers that have been entered
+#using while loop, continuously ask for the user until invalid input
+#check if the input is a valid number
+#check if the number is a duplicate or unique
+#print invalid if the number is invalid
