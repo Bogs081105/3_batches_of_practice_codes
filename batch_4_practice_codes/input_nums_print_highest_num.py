4babb8a0564011c1a622bc8b963755b93855f4ab
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 21:26:09 +0800

    added try and except functions to exclude invalid inputs

--- batch_4_practice_codes/input_nums_print_highest_num.py
+++ batch_4_practice_codes/input_nums_print_highest_num.py
@@ -2,8 +2,12 @@
 nums = []
 #ask the user to input nums continuously using while 
 while True:
-    num = int(input("Enter a number(input invalid item to stop): "))
-#append the nums to the list
-    nums.append(num)
+    try:
+        num = int(input("Enter a number(input invalid item to stop): "))
+#append the numbers to the list
+        nums.append(num)
 #add a function that excludes invalid inputs
+    except ValueError:
+        print("Invalid Input")
+        break
 #find and display the highest number
