e2427f01be167394a53c0590e74ba348b1ae8ac6
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 22:34:27 +0800

    added an input number continuously function

--- batch_4_practice_codes/input_until_invalid_print_average.py
+++ batch_4_practice_codes/input_until_invalid_print_average.py
@@ -1,6 +1,9 @@
 #create an empty list
 nums = []
 #ask continuously for numbers using while loop
+while True:
+    try:
+        num = int(input("Enter a number(Input an invalid item to stop): "))
 #append numbers into list
 #stop if invalid number
 #calculate the average
