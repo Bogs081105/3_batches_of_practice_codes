86de022d8d02b1b5bec1722f7236eefc7354d195
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 17:50:36 +0800

    added a function which inputs numbers continuously

--- batch_3_practice_codes/input_until_invalid_print_lowest_num.py
+++ batch_3_practice_codes/input_until_invalid_print_lowest_num.py
@@ -1,6 +1,8 @@
 #set a variable to store the lowest number
 lowest_num = None
 #using while loop, input numbers continuously
+while True:
+    user_input = input("Enter a number: ")
 #create a function that identifies which is the lowest number
 #break if it is an invalid number
 #print the lowest number
