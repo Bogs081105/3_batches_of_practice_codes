64291228beabcb11d6a9b32b4b4ece1a5f85b2e5
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 20:34:53 +0800

    added a for loop to input 10 numbers

--- batch_4_practice_codes/input_10_nums_print_all_dupes.py
+++ batch_4_practice_codes/input_10_nums_print_all_dupes.py
@@ -1,6 +1,8 @@
-#initialize an empty set
+#initialize an empty list
 nums = []
 #using for loop, ask the user for 10 nums
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
 #append the inputs inside the set
 #find the duplicates
 #print results
