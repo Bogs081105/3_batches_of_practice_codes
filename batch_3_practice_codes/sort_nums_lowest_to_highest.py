341751f2a9eeed785a9ebc4a0973ab5d82521428
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 20:08:21 +0800

    appended numbers to empty set to store

--- batch_3_practice_codes/sort_nums_lowest_to_highest.py
+++ batch_3_practice_codes/sort_nums_lowest_to_highest.py
@@ -4,6 +4,7 @@ nums = []
 while True:
     num = int(input("Enter a number(enter non-numeric value to stop): "))
 #append into list
+    nums.append(num)
 #stop if invalid number
 #sort the numbers from lowest to highest
 #print the sorted numbers
