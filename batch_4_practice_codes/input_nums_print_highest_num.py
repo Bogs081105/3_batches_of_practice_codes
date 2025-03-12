750d60153883f3c8a76ffea36b78e692ea94e656
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 21:20:34 +0800

    added an empty set

--- batch_4_practice_codes/input_nums_print_highest_num.py
+++ batch_4_practice_codes/input_nums_print_highest_num.py
@@ -1,8 +1,6 @@
 #initialize an empty list to store the numbers
-numbers = []
+nums = []
 #ask the user to input nums continuously using while loop
-while True:
-    num = int(input("Enter a number(input invalid item to stop): "))
 #append the nums to the list
 #add a function that excludes invalid inputs
 #find and display the highest number
