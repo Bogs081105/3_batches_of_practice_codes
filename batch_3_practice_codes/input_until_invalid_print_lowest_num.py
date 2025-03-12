fb4961bc2a72265657c12adad2aba6fd54264520
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 17:42:05 +0800

    added a lowest number variable

--- batch_3_practice_codes/input_until_invalid_print_lowest_num.py
+++ batch_3_practice_codes/input_until_invalid_print_lowest_num.py
@@ -1,4 +1,5 @@
-#set a variable to store the lowest numberw
+#set a variable to store the lowest number
+lowest_num = None
 #using while loop, input numbers continuously
 #create a function that identifies which is the lowest number
 #break if it is an invalid number
