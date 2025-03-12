21ac2525478faabec5fc673607c7d35059a968aa
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 16:32:54 +0800

    swapped the order of the pseudo code which set the duplicate or unique function first

--- batch_3_practice_codes/display_unique_and_duplicate.py
+++ batch_3_practice_codes/display_unique_and_duplicate.py
@@ -3,6 +3,6 @@ seen_nums = set()
 #using while loop, continuously ask for the user until invalid input
 while True:
     input = input("Please enter a number: ")
-#check if the input is a valid number
 #check if the number is a duplicate or unique
+#check if the input is a valid number
 #print invalid if the number is invalid
