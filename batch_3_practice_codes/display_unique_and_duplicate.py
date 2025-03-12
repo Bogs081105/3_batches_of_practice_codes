934756452b3119265097e80cabd8d2c49553e6ad
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 16:25:32 +0800

    added a function where it stores numbers that have been seen

--- batch_3_practice_codes/display_unique_and_duplicate.py
+++ batch_3_practice_codes/display_unique_and_duplicate.py
@@ -1,4 +1,5 @@
 #use set function to store numbers that have been entered
+seen_nums = set()
 #using while loop, continuously ask for the user until invalid input
 #check if the input is a valid number
 #check if the number is a duplicate or unique
