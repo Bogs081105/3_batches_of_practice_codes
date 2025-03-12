59e45f17727c19ddb9b5b22edd4ba3d156fd3b0f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 16:30:11 +0800

    added a while loop to continuously store numbers

--- batch_3_practice_codes/display_unique_and_duplicate.py
+++ batch_3_practice_codes/display_unique_and_duplicate.py
@@ -1,6 +1,8 @@
 #use set function to store numbers that have been entered
 seen_nums = set()
 #using while loop, continuously ask for the user until invalid input
+while True:
+    input = input("Please enter a number: ")
 #check if the input is a valid number
 #check if the number is a duplicate or unique
 #print invalid if the number is invalid
