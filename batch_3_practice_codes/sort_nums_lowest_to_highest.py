32a3ff6a4861bd08e6e27ff97126953241ce2fc2
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 19:57:31 +0800

    added pseudo code

---
+++ batch_3_practice_codes/sort_nums_lowest_to_highest.py
@@ -0,0 +1,7 @@
+#initialize an emptylist
+num = []
+#using while loop, ask the user to input the numbers
+#append into list
+#stop if invalid number
+#sort the numbers from lowest to highest
+#print the sorted numbers
