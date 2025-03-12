ccde509981fc0623b9cbd9d2128ffe3888cc3b45
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 20:58:28 +0800

    invalid input function added

--- batch_4_practice_codes/input_until_invalid_print_most_dupes.py
+++ batch_4_practice_codes/input_until_invalid_print_most_dupes.py
@@ -2,8 +2,13 @@
 nums = []
 #continuously ask the user for nums until invalid input
 while True:
-    num = int(input("Enter a number (enter an invalid item to stop): "))
+    try:
+        num = int(input("Enter a number (enter an invalid item to stop): "))
 #append to list
-    
+        nums.append(num)
+#stop if an invalid input is entered
+    except ValueError:
+        print("Invalid Input")
+        break
 #find the number with the most duplicates
 #print the result
