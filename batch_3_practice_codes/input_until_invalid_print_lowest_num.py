62aad4106b44fcb77ba1930576468f26a6aae8ab
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 18:01:48 +0800

    added a function which identifies the lowest number in the input

--- batch_3_practice_codes/input_until_invalid_print_lowest_num.py
+++ batch_3_practice_codes/input_until_invalid_print_lowest_num.py
@@ -4,5 +4,12 @@ lowest_num = None
 while True:
     user_input = input("Enter a number: ")
 #create a function that identifies which is the lowest number
+    try:
+        num = float(user_input)
+        if lowest_num is None or num < lowest_num:
+            lowest_num = num
+    except ValueError:
+        break
+
 #break if it is an invalid number
 #print the lowest number
