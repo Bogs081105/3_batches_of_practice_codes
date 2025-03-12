618610298b5648255aa5a996ab7b4dca48e0ddd6
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-12 20:40:11 +0800

    added a function to find the duplicates

--- batch_4_practice_codes/input_10_nums_print_all_dupes.py
+++ batch_4_practice_codes/input_10_nums_print_all_dupes.py
@@ -4,5 +4,7 @@ nums = []
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
 #append the inputs inside the set
+    nums.append(num)
 #find the duplicates
+dupes = set([num for num in nums if nums.count(num > 1)])
 #print results
